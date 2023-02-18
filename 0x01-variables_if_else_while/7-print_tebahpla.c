ff7aa64c0077241676a634f90edef9911aac009b
FAVOUR <enufavour735@gmail.com>
2023-02-18 09:52:43 -0800

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry
+* Return: always 0
+*/
+int main(void)
+{
+	char n = 'z';
+
+	while (n >= 'a')
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
