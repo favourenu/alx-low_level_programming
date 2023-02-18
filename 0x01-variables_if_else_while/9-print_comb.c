9f57ec99a65d317ba6b3a585b69ab0f8081d6910
FAVOUR <enufavour735@gmail.com>
2023-02-18 11:50:14 -0800

    everything cos of 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,20 +5,16 @@
 */
 int main(void)
 {
-	char c = ',';
-	char s = ' ';
-	int n = 0;
+	int n;
 
-	while (n <= 9)
+	for (n = 0; n < 10; n++)
 	{
-		putchar(48 + n);
-		if (n != 9)
+		putchar(n + '0');
+		if (n < 9)
 		{
-			putchar(c);
+			putchar(',');
+			putchar(' ');
 		}
-
-		putchar(s);
-		n++;
 	}
 	putchar('\n');
 	return (0);
