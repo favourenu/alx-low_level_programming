fdc841c7c8ad3cbb4880b1e45fccb854a7b6fad0
FAVOUR <enufavour735@gmail.com>
2023-02-22 15:20:39 -0800

    creating function that calls a to z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_alphabet - listing a to z in lowercase
+*/
+void print_alphabet(void)
+{
+	char lett;
+
+	for(lett = 'a'; lett <= 'z'; lett++)
+	_putchar(lett);
+
+	_putchar('\n');
+}
