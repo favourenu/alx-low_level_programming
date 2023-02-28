1dbbae5a5be754ddd289d58ca0fb497e01385aa3
favour <enufavour735@gmail.com>
2023-02-28 15:33:48 +0100

    reverse strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.c"
+/**
+ * rev_string - reverse a string.
+ * @s: the parameter
+ */
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int counter = 0;
+	int i;
+
+	while (s[counter] != '\n')
+	counter++;
+	for (i = 0; i < counter; i++)
+	{
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
+	}
+}
+
