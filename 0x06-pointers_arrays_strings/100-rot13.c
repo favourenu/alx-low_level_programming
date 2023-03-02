56a569a9949fcf9c61c020fb4aa02760eefaa101
favour <enufavour735@gmail.com>
2023-03-02 18:25:23 +0100

    still encoding with rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - encode a string
+ * @s: parameter
+ * Return: s
+ */
+char *rot13(char *s)
+{
+	int e;
+	int f;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (e = 0; s[e] != '\0'; e++)
+	{
+		for (f = 0; f < 52; f++)
+		{
+			if (s[e] == data1[f])
+			{
+				s[e] = datarot[f];
+				breat;
+			}
+		}
+	}
+	return (s);
+}
