1b27a4aadceed556ef7037d09e109d1307b590f0
favour <enufavour735@gmail.com>
2023-03-13 02:25:08 +0100

    the argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - the function
+ * @argv: the parameter
+ * @argc: the parameter
+ * Return: 0
+ */
+int main(int argc, char *argv)
+{
+	printf("%d\n", argc);
+	printf("%s\n", argv);
+	return (0);
+}
