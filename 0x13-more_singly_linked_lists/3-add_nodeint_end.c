acdbbde8abca9a82a0675dcff39eb2114feac85e
favour <enufavour735@gmail.com>
2023-04-05 00:19:42 +0100

    the 3 again

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -6,7 +6,7 @@
  *    * @n: data to insert in the new element
  *     *
  *      * Return: pointer to the new node, or NULL if it fails
- *       */
+*/
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
